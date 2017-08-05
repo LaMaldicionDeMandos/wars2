275a56ce5ceb0a882202d924aba4ef0e1accafb3
Marcelo Pasut <pasutmarcelo@gmail.com>
2017-08-05 20:34:09 -0300

    playing with angular

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  {id: 1, name: 'Goku'},
+  {id: 2, name: 'Vegeta'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'El titulo';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
